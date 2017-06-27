66374c1bdc674957ac2a846b785ffeb54fe63cb5
elizabethprins <elizabethprins@gmail.com>
2017-06-27 13:28:13 +0200

    setup store and api client, wrap everything in mui theme provider, add dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+// src/index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute } from 'react-router'
+import store, { history } from './store'
+import registerServiceWorker from './registerServiceWorker'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App'
+//import components
+
+import './index.css'
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        // <IndexRoute component={signin?} />
+        // <Route path="url" component={comp-name} />
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
+
+registerServiceWorker()
