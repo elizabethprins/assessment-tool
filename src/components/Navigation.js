12393ea419097658484e0ded84accac6a4a42d87
elizabethprins <elizabethprins@gmail.com>
2017-06-27 14:06:30 +0200

    add user sign-in component, user actions and reducer, loading stuff, mui appbar

---
+++ src/components/Navigation.js
@@ -0,0 +1,40 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+import signOut from '../../actions/user/sign-out'
+import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import Assessment from 'material-ui/svg-icons/action/assessment'
+import FlatButton from 'material-ui/FlatButton'
+
+class Navigation extends PureComponent {
+  static propTypes = {
+    signedIn: PropTypes.bool,
+    push: PropTypes.func.isRequired,
+    signOut: PropTypes.func.isRequired,
+  }
+
+  goHome = () => {
+    this.props.push('/')
+  }
+
+  render() {
+    const { signedIn, signOut } = this.props
+    return (
+      <AppBar
+        title="Student Assessment"
+        iconElementLeft={<IconButton onClick={this.goHome}><Assessment /></IconButton>}
+        iconElementRight={signedIn ?
+          <FlatButton label="Sign out" onClick={signOut} />
+        }
+      />
+    )
+  }
+}
+
+const mapStateToProps = ({ currentUser }) => ({
+  signedIn: !!currentUser && !!currentUser._id,
+})
+
+export default connect(mapStateToProps, { push, signOut })(Navigation)
